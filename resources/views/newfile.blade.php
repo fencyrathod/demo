342273727163fe1c8609efe25dadeb422eacb378
fancy rathod <fancy@gmail.com>
2023-12-04 15:57:29 +0530

    new changes

--- resources/views/newfile.blade.php
+++ resources/views/newfile.blade.php
@@ -1,1 +1,14 @@
-<h1>This is my new file</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="form-group">
+        <label class="form-label">Name</label>
+        <input type="text" name="name" class="form-control">
+    </div>
+</body>
+</html>
